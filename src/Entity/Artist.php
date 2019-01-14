dc5bf99b850942db538540fed7c11d3f03c64f10
Arun Basnet <>
2019-01-14 19:34:51 +0100

    Created Artist Entity and Repository

---
+++ src/Entity/Artist.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ArtistRepository")
+ */
+class Artist
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $artist_name;
+
+    /**
+     * @ORM\Column(type="string", length=6)
+     */
+    private $artist_token;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Album", mappedBy="artist_id")
+     */
+    private $albums;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Song", mappedBy="artist_id", orphanRemoval=true)
+     */
+    private $songs;
+
+    public function __construct()
+    {
+        $this->albums = new ArrayCollection();
+        $this->songs = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getArtistName(): ?string
+    {
+        return $this->artist_name;
+    }
+
+    public function setArtistName(string $artist_name): self
+    {
+        $this->artist_name = $artist_name;
+
+        return $this;
+    }
+
+    public function getArtistToken(): ?string
+    {
+        return $this->artist_token;
+    }
+
+    public function setArtistToken(string $artist_token): self
+    {
+        $this->artist_token = $artist_token;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Album[]
+     */
+    public function getAlbums(): Collection
+    {
+        return $this->albums;
+    }
+
+    public function addAlbum(Album $album): self
+    {
+        if (!$this->albums->contains($album)) {
+            $this->albums[] = $album;
+            $album->addArtistId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAlbum(Album $album): self
+    {
+        if ($this->albums->contains($album)) {
+            $this->albums->removeElement($album);
+            $album->removeArtistId($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Song[]
+     */
+    public function getSongs(): Collection
+    {
+        return $this->songs;
+    }
+
+    public function addSong(Song $song): self
+    {
+        if (!$this->songs->contains($song)) {
+            $this->songs[] = $song;
+            $song->setArtistId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSong(Song $song): self
+    {
+        if ($this->songs->contains($song)) {
+            $this->songs->removeElement($song);
+            // set the owning side to null (unless already changed)
+            if ($song->getArtistId() === $this) {
+                $song->setArtistId(null);
+            }
+        }
+
+        return $this;
+    }
+}
