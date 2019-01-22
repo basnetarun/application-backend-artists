f5aa39da14dfdf2cea95112f24c52fb495a83e34
Arun Basnet <>
2019-01-22 16:09:24 +0100

    Formatting and Serializer issue

--- src/Controller/ArtistController.php
+++ src/Controller/ArtistController.php
@@ -10,8 +10,10 @@ use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Routing\ClassResourceInterface;
 use Symfony\Component\HttpFoundation\Response;
 
+
 use App\Repository\ArtistRepository;
 use Doctrine\ORM\EntityManagerInterface;
+
 
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
@@ -49,14 +51,20 @@ class ArtistController extends FOSRestController implements ClassResourceInterfa
     {
         
         $artists = $this->artistRepository->findAll();
-
-        if(null == $artists):
+        
+        if(null == $artists) {
             throw new NotFoundHttpException('Es sind zurzeit keine Artisten in Datenbank vorhanden.');
-        endif;
-       
+        }
+        
+        $result = array();
+        foreach($artists as $artist) {
+            array_push($result, $this->getArtistData($artist));
+            
+        }
+        
         return $this->view(
             [   
-                'data' => new Serializer($artists),
+                'data' => $result,
                 'status' => Response::HTTP_OK
             ]
         );
@@ -66,27 +74,61 @@ class ArtistController extends FOSRestController implements ClassResourceInterfa
     /** 
      * Retrieves a single Artist resource
      * @Rest\Get(
-     *      "/artist/{artistId}.{ext}"
+     *      "/artist/{artistToken}.{ext}"
      * )
      * @return Artist|null
      * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
      */
 
-    public function getArtist(int $artistId)
+    public function getArtist(string $artistToken)
     {
-        $artist = $this->artistRepository->findById($artistId);
+        $artist = $this->artistRepository->findOneBy(['artist_token' => $artistToken]);
+        
 
-        if(null == $artist):
+        if(null == $artist) {
             throw new NotFoundHttpException('Es wurden keinen Artist mit der ID '.$artistId.' gefunden.');
-        endif;
-
+        }
+        $result = array();
+        array_push($result, $this->getArtistData($artist));
         return $this->view(
             [   
-                $artist,
+                'data' => $result,
                 'status' => Response::HTTP_OK
             ]
         );
     }
+
+
+    /**
+     * Sets Artists as Array with onyl the required fields
+     * @return Collection|Artist[]
+     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+     */
+
+     private function getArtistData(\App\Entity\Artist $obj) 
+     {
+        $artistData = array(); 
+        
+        if($obj != null) {
+            
+            $artistData['token']    = $obj->getArtistToken();
+            $artistData['name']     = $obj->getArtistName();
+            
+            $albums = $obj->getAlbums();
+            if($albums != null) {
+                foreach($albums as $album) {
+                    $artistData['albums'][] = array(
+                        'token'       => $album->getAlbumToken(),
+                        'album_title' => $album->getAlbumTitle(),
+                        'album_cover' => $album->getAlbumCover()
+
+                    );
+                }
+            }
+        }
+
+        return $artistData;
+     }
 
 
 }
